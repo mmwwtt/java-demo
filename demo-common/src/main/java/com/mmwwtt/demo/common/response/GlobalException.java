303a661ec860f9caa14b7297c4e99bd131a83af2
moweitao <2310607361@qq.com>
2024-07-02 01:58:19 +0800

    validation修改

--- demo-common/src/main/java/com/mmwwtt/demo/common/response/GlobalException.java
+++ demo-common/src/main/java/com/mmwwtt/demo/common/response/GlobalException.java
@@ -1,5 +1,6 @@
 package com.mmwwtt.demo.common.response;
 
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +9,8 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @Slf4j
 @RestControllerAdvice
@@ -25,10 +28,18 @@ public class GlobalException {
     @ExceptionHandler({MethodArgumentNotValidException.class})
     public ApiResponse<Void> exception(MethodArgumentNotValidException e) {
         BindingResult bindingResult = e.getBindingResult();
-        ObjectError objectError = bindingResult.getAllErrors().stream().findFirst().get();
-        String messages = objectError.getDefaultMessage();
-        return ApiResponse.fail(ResponseCode.ERROR.getCode(), messages);
+        List<String> errorMessageList = bindingResult.getAllErrors()
+                .stream().map(ObjectError::getDefaultMessage)
+                .toList();
+        return ApiResponse.fail(ResponseCode.ERROR.getCode(), errorMessageList.toString());
     }
+
+    @ExceptionHandler({ConstraintViolationException.class})
+    public ApiResponse<Void> exception(ConstraintViolationException e) {
+        return ApiResponse.fail(ResponseCode.ERROR.getCode(), e.getMessage());
+    }
+
+
 
     @ExceptionHandler(value = CustomException.class)
     public ApiResponse<Void> CustomExceptionHandler(CustomException e) {
--- demo-ee-8000/src/main/java/com/mmwwtt/demo/ee/validation/ValidationDemo.java
+++ demo-ee-8000/src/main/java/com/mmwwtt/demo/ee/validation/ValidationDemo.java
@@ -1,10 +1,17 @@
 package com.mmwwtt.demo.ee.validation;
 
 import com.mmwwtt.demo.common.response.ApiResponse;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/service/demo/ee")
@@ -17,7 +24,15 @@ public class ValidationDemo {
 
     @PostMapping("/validation1")
     public ApiResponse<Void> demoValidation1( BaseInfoCreateDTO baseInfoCreateDTO) {
-        demoValidation(baseInfoCreateDTO);
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        validator.validate(baseInfoCreateDTO);
+
+        Set<ConstraintViolation<BaseInfoCreateDTO>> violations = validator.validate(baseInfoCreateDTO);
+        if (!violations.isEmpty()) {
+            throw new ConstraintViolationException(new HashSet<>(violations));
+        }
+
+
         return ApiResponse.success();
     }
 }
