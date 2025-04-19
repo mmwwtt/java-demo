8902e1ca457b9ad1d42a7393181a08553dff1179
moweitao <2310607361@qq.com>
2025-04-19 15:03:18 +0800

    2025-04-19  新建微服务项目

--- 8201-demo-nacos-service2/src/main/java/nacos/demo/DemoController.java
+++
@@ -1,9 +0,0 @@
-package nacos.demo;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/nacos")
-public class DemoController {
-}
