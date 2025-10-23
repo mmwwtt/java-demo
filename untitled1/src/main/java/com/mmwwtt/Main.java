c33568a4ea1d0686edf80fe458dc04b8dc1a3af4
mmwwtt <2310607361@qq.com>
2025-10-23 08:06:26 +0800

    股票接口

--- untitled1/src/main/java/com/mmwwtt/Main.java
+++ untitled1/src/main/java/com/mmwwtt/Main.java
@@ -11,10 +11,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static com.mmwwtt.common.Constants.*;
 
@@ -24,28 +21,95 @@ import static com.mmwwtt.common.Constants.*;
  */
 public class Main {
     @Test
-   public void test(){
-        RestTemplate restTemplate=new RestTemplate();
-         restTemplate.setInterceptors(Collections.singletonList(new LoggingInterceptor()));
+    public void test() throws InterruptedException {
+        RestTemplate restTemplate = new RestTemplate();
+        //restTemplate.setInterceptors(Collections.singletonList(new LoggingInterceptor()));
         Map<String, String> map = new HashMap<>();
         map.put(LICENCE, BI_YING_LICENCE);
         ResponseEntity<List<Stock>> stocksListResponse = restTemplate.exchange(STOCK_LIST_URL, HttpMethod.GET,
-                null, new ParameterizedTypeReference<>() {},map);
+                null, new ParameterizedTypeReference<>() {
+                }, map);
         List<Stock> stockList = stocksListResponse.getBody();
 
+        int cnt = 0;
+        for (Stock stock : stockList) {
+            cnt++;
+            if (cnt > 200) {
+                Thread.sleep(60000);
+                cnt = 0;
+            }
+            Thread.sleep(100);
+            Map<String, String> map1 = new HashMap<>();
+            map1.put(LICENCE, BI_YING_LICENCE);
+            map1.put(STOCK_CODE, stock.getDm());
+            map1.put(TIME_LEVEL, TimeLevelEnum.DAY.getCode());
+            map1.put(EXCLUDE_RIGHT, ExcludeRightEnum.NONE.getCode());
+            map1.put(START_DATA, "20251001");
+            map1.put(END_DATA, "20251025");
+            map1.put(MAX_SIZE, "30");
+            ResponseEntity<List<StockDetail>> stockDetailResponse=null;
+            try {
+                stockDetailResponse = restTemplate.exchange(HISTORY_DATA_URL, HttpMethod.GET,
+                        null, new ParameterizedTypeReference<>() {
+                        }, map1);
+            } catch (Exception e) {
+                continue;
+            }
+            List<StockDetail> stockDetailList = stockDetailResponse.getBody();
+            boolean isMoreQuantityRed = isMoreQuantityRed(stockDetailList);
+            boolean isLessQuantityRed = isLessQuantityGreen(stockDetailList);
+            if (isMoreQuantityRed || isLessQuantityRed) {
+                System.out.printf("%s_%s\n", stock.getDm(), stock.getMc());
+            }
+        }
+    }
 
-        Map<String, String> map1 = new HashMap<>();
-        map1.put(LICENCE, BI_YING_LICENCE);
-        map1.put(STOCK_CODE, "000001");
-        map1.put(TIME_LEVEL, TimeLevelEnum.DAY.getCode());
-        map1.put(EXCLUDE_RIGHT, ExcludeRightEnum.NONE.getCode());
-        map1.put(START_DATA, "20221020");
-        map1.put(END_DATA, "20251020");
-        map1.put(MAX_SIZE, "365");
-        ResponseEntity<List<StockDetail>> stockDetailResponse = restTemplate.exchange(HISTORY_DATA_URL, HttpMethod.GET,
-                null, new ParameterizedTypeReference<>() {},map1);
-        List<StockDetail> stockDetailList = stockDetailResponse.getBody();
+    /**
+     * 放量红 当天收盘价高于前一天收盘价，且成交量比前一天高
+     *
+     * @return
+     */
+    public boolean isMoreQuantityRed(List<StockDetail> stockDetailList) {
+        if (Objects.isNull(stockDetailList) || stockDetailList.size() < 3) {
+            return false;
+        }
+        StockDetail stockDetail3 = stockDetailList.get(stockDetailList.size() - 3);
+        StockDetail stockDetail2 = stockDetailList.get(stockDetailList.size() - 2);
+        StockDetail stockDetail1 = stockDetailList.get(stockDetailList.size() - 1);
+        return quentityIsMoreThan(stockDetail2.getV(), stockDetail3.getV())
+                && stockDetail2.getC() - stockDetail3.getC() > 0
+                && quentityIsMoreThan(stockDetail1.getV(), stockDetail2.getV())
+                && stockDetail1.getC() - stockDetail2.getC() > 0;
+    }
 
-        System.out.println();
+    /**
+     * 缩量绿 当天收盘价低于前一天收盘价，且成交量比前一天低， 但是前面两天都是放量红
+     *
+     * @return
+     */
+    public boolean isLessQuantityGreen(List<StockDetail> stockDetailList) {
+        if (Objects.isNull(stockDetailList) || stockDetailList.size() < 4) {
+            return false;
+        }
+        StockDetail stockDetail4 = stockDetailList.get(stockDetailList.size() - 4);
+        StockDetail stockDetail3 = stockDetailList.get(stockDetailList.size() - 3);
+        StockDetail stockDetail2 = stockDetailList.get(stockDetailList.size() - 2);
+        StockDetail stockDetail1 = stockDetailList.get(stockDetailList.size() - 1);
+        if (!isMoreQuantityRed(Arrays.asList(stockDetail4, stockDetail3, stockDetail2))) {
+            return false;
+        }
+        return stockDetail1.getV() - stockDetail2.getV() < 0
+                && stockDetail1.getC() - stockDetail2.getC() < 0;
+    }
+
+    /**
+     * 比较成交量是否相同，运行5%的误差
+     *
+     * @param num1
+     * @param num2
+     * @return
+     */
+    private boolean quentityIsMoreThan(float num1, float num2) {
+        return Math.abs(num1 - num2) / num1 < 0.05 || num1 > num2;
     }
 }
--- untitled1/src/main/java/com/mmwwtt/entity/StockDetail.java
+++ untitled1/src/main/java/com/mmwwtt/entity/StockDetail.java
@@ -13,4 +13,39 @@ public class StockDetail {
      * 开盘价
      */
     private float o;
+
+    /**
+     * 最高价
+     */
+    private float h;
+
+    /**
+     * 最低价
+     */
+    private float l;
+
+    /**
+     * 收盘价
+     */
+    private float c;
+
+    /**
+     * 成交量
+     */
+    private float v;
+
+    /**
+     * 成交额
+     */
+    private float a;
+
+    /**
+     * 前收盘价
+     */
+    private float pc;
+
+    /**
+     * 1停牌；  0不停牌
+     */
+    private int sf;
 }
