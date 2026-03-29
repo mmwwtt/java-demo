a5726d4d483b9c5da8d8a29a40b4a6803ccbfb3f
moweitao <2310607361@qq.com>
2026-03-29 15:51:29 +0800

    update

--- 8301-stock/src/main/java/com/mmwwtt/stock/enums/FilterFildEnum.java
+++ 8301-stock/src/main/java/com/mmwwtt/stock/enums/FilterFildEnum.java
@@ -23,21 +23,21 @@ public enum FilterFildEnum implements BaseEnum {
             Strategy::getRise5MaxMiddle,
             (StrategyTmp tmp) -> {
                 int level = tmp.getStrategyCodeSet().size();
-                if (lessThan(tmp.getPert(), multiply(tmp.getParentPert(), 1.01))
-                        || (level == 2 && (lessThan(tmp.getPert(), 0.065) || tmp.getDateCnt() < 60 || tmp.getRise5MinMiddle() < -0.06))
-                        || (level == 3 && (lessThan(tmp.getPert(), 0.075) || tmp.getDateCnt() < 60 || tmp.getRise5MinMiddle() < -0.055))
-                        || (level == 4 && (lessThan(tmp.getPert(), 0.08) || tmp.getDateCnt() < 55 || tmp.getRise5MinMiddle() < -0.05))
-                        || (level == 5 && (lessThan(tmp.getPert(), 0.09) || tmp.getDateCnt() < 55 || tmp.getRise5MinMiddle() < -0.05))
-                        || (level == 6 && (lessThan(tmp.getPert(), 0.10) || tmp.getDateCnt() < 55 || tmp.getRise5MinMiddle() < -0.05))
-                        || (level == 7 && (lessThan(tmp.getPert(), 0.11) || tmp.getDateCnt() < 50 || tmp.getRise5MinMiddle() < -0.045))
-                        || (level == 8 && (lessThan(tmp.getPert(), 0.12) || tmp.getDateCnt() < 50 || tmp.getRise5MinMiddle() < -0.045))
-                        || (level == 9 && (lessThan(tmp.getPert(), 0.125) || tmp.getDateCnt() < 45 || tmp.getRise5MinMiddle() < -0.04))
-                        || (level == 10 && (lessThan(tmp.getPert(), 0.13) || tmp.getDateCnt() < 45 || tmp.getRise5MinMiddle() < -0.04))
-                        || (level == 11 && (lessThan(tmp.getPert(), 0.135) || tmp.getDateCnt() < 45 || tmp.getRise5MinMiddle() < -0.04))
-                        || (level == 12 && (lessThan(tmp.getPert(), 0.14) || tmp.getDateCnt() < 40 || tmp.getRise5MinMiddle() < -0.04))
-                        || (level == 13 && (lessThan(tmp.getPert(), 0.145) || tmp.getDateCnt() < 35 || tmp.getRise5MinMiddle() < -0.04))
-                        || (level == 14 && (lessThan(tmp.getPert(), 0.15) || tmp.getDateCnt() < 30 || tmp.getRise5MinMiddle() < -0.04))
-                        || (level == 15 && (lessThan(tmp.getPert(), 0.155) || tmp.getDateCnt() < 30 || tmp.getRise5MinMiddle() < -0.04))) {
+                if (lessThan(tmp.getPert(), multiply(tmp.getParentPert(), 1.001))
+                        || (level == 2 && (lessThan(tmp.getPert(), 0.07) || tmp.getDateCnt() < 60 || tmp.getRise5MinMiddle() < -0.055))
+                        || (level == 3 && (lessThan(tmp.getPert(), 0.075) || tmp.getDateCnt() < 60 || tmp.getRise5MinMiddle() < -0.05))
+                        || (level == 4 && (lessThan(tmp.getPert(), 0.08) || tmp.getDateCnt() < 55 || tmp.getRise5MinMiddle() < -0.045))
+                        || (level == 5 && (lessThan(tmp.getPert(), 0.09) || tmp.getDateCnt() < 55 || tmp.getRise5MinMiddle() < -0.045))
+                        || (level == 6 && (lessThan(tmp.getPert(), 0.10) || tmp.getDateCnt() < 55 || tmp.getRise5MinMiddle() < -0.045))
+                        || (level == 7 && (lessThan(tmp.getPert(), 0.11) || tmp.getDateCnt() < 50 || tmp.getRise5MinMiddle() < -0.04))
+                        || (level == 8 && (lessThan(tmp.getPert(), 0.12) || tmp.getDateCnt() < 50 || tmp.getRise5MinMiddle() < -0.04))
+                        || (level == 9 && (lessThan(tmp.getPert(), 0.125) || tmp.getDateCnt() < 45 || tmp.getRise5MinMiddle() < -0.035))
+                        || (level == 10 && (lessThan(tmp.getPert(), 0.13) || tmp.getDateCnt() < 45 || tmp.getRise5MinMiddle() < -0.035))
+                        || (level == 11 && (lessThan(tmp.getPert(), 0.135) || tmp.getDateCnt() < 45 || tmp.getRise5MinMiddle() < -0.035))
+                        || (level == 12 && (lessThan(tmp.getPert(), 0.14) || tmp.getDateCnt() < 40 || tmp.getRise5MinMiddle() < -0.035))
+                        || (level == 13 && (lessThan(tmp.getPert(), 0.145) || tmp.getDateCnt() < 40 || tmp.getRise5MinMiddle() < -0.035))
+                        || (level == 14 && (lessThan(tmp.getPert(), 0.15) || tmp.getDateCnt() < 40 || tmp.getRise5MinMiddle() < -0.035))
+                        || (level == 15 && (lessThan(tmp.getPert(), 0.155) || tmp.getDateCnt() < 40 || tmp.getRise5MinMiddle() < -0.035))) {
                     return false;
                 }
                 return true;
