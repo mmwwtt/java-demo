94bdb6823501bb33168fe55508010368a722b013
moweitao <2310607361@qq.com>
2026-04-09 23:39:07 +0800

    update

--- 8301-stock/src/main/java/com/mmwwtt/stock/enums/FilterFieldEnum.java
+++ 8301-stock/src/main/java/com/mmwwtt/stock/enums/FilterFieldEnum.java
@@ -410,7 +410,7 @@ public enum FilterFieldEnum implements BaseEnum {
 
 
     RISE3_MIDDLE_30_DAY("rise3Middle30Day", "3日涨幅中位数",
-            3, 70,
+            13, 70,
             Detail::getRise3,
             Detail::getRise3Min,
             StrategyL1::getRise3Middle,
@@ -441,62 +441,62 @@ public enum FilterFieldEnum implements BaseEnum {
                 }
                 switch (level) {
                     case 2:
-                        if (lessThan(tmp.getMiddle(), 0.008) && tmp.getMiddle() + tmp.getMinMiddle() * 0.5 < 0) {
+                        if (lessThan(tmp.getMiddle(), 0.007) && tmp.getMiddle() + tmp.getMinMiddle() * 0.5 < 0) {
                             return false;
                         }
                         break;
                     case 3:
-                        if (lessThan(tmp.getMiddle(), 0.01) && tmp.getMiddle() + tmp.getMinMiddle() * 0.6 < 0) {
+                        if (lessThan(tmp.getMiddle(), 0.010) && tmp.getMiddle() + tmp.getMinMiddle() * 0.7 < 0) {
                             return false;
                         }
                         break;
                     case 4:
-                        if (lessThan(tmp.getMiddle(), 0.07) && tmp.getMiddle() + tmp.getMinMiddle() * 1.8 < 0) {
+                        if (lessThan(tmp.getMiddle(), 0.015) && tmp.getMiddle() + tmp.getMinMiddle() * 0.8 < 0) {
                             return false;
                         }
                         break;
                     case 5:
-                        if (lessThan(tmp.getMiddle(), 0.08) && tmp.getMiddle() + tmp.getMinMiddle() * 1.9 < 0) {
+                        if (lessThan(tmp.getMiddle(), 0.020) && tmp.getMiddle() + tmp.getMinMiddle() * 0.9 < 0) {
                             return false;
                         }
                         break;
                     case 6:
-                        if (lessThan(tmp.getMiddle(), 0.9) && tmp.getMiddle() + tmp.getMinMiddle() * 2.0 < 0) {
+                        if (lessThan(tmp.getMiddle(), 0.025) && tmp.getMiddle() + tmp.getMinMiddle() * 1.0 < 0) {
                             return false;
                         }
                         break;
                     case 7:
-                        if (lessThan(tmp.getMiddle(), 0.10) && tmp.getMiddle() + tmp.getMinMiddle() * 2.1 < 0) {
+                        if (lessThan(tmp.getMiddle(), 0.030) && tmp.getMiddle() + tmp.getMinMiddle() * 1.1 < 0) {
                             return false;
                         }
                         break;
                     case 8:
-                        if (lessThan(tmp.getMiddle(), 0.11) && tmp.getMiddle() + tmp.getMinMiddle() * 2.2 < 0) {
+                        if (lessThan(tmp.getMiddle(), 0.035) && tmp.getMiddle() + tmp.getMinMiddle() * 1.2 < 0) {
                             return false;
                         }
                         break;
                     case 9:
-                        if (lessThan(tmp.getMiddle(), 0.115) && tmp.getMiddle() + tmp.getMinMiddle() * 2.3 < 0) {
+                        if (lessThan(tmp.getMiddle(), 0.04) && tmp.getMiddle() + tmp.getMinMiddle() * 1.3 < 0) {
                             return false;
                         }
                         break;
                     case 10:
-                        if (lessThan(tmp.getMiddle(), 0.12) && tmp.getMiddle() + tmp.getMinMiddle() * 2.4 < 0) {
+                        if (lessThan(tmp.getMiddle(), 0.045) && tmp.getMiddle() + tmp.getMinMiddle() * 1.4 < 0) {
                             return false;
                         }
                         break;
                     case 11:
-                        if (lessThan(tmp.getMiddle(), 0.125) && tmp.getMiddle() + tmp.getMinMiddle() * 2.5 < 0) {
+                        if (lessThan(tmp.getMiddle(), 0.05) && tmp.getMiddle() + tmp.getMinMiddle() * 1.5 < 0) {
                             return false;
                         }
                         break;
                     case 12:
-                        if (lessThan(tmp.getMiddle(), 0.13) && tmp.getMiddle() + tmp.getMinMiddle() * 2.6 < 0) {
+                        if (lessThan(tmp.getMiddle(), 0.055) && tmp.getMiddle() + tmp.getMinMiddle() * 1.6 < 0) {
                             return false;
                         }
                         break;
                     case 13:
-                        if (lessThan(tmp.getMiddle(), 0.135) && tmp.getMiddle() + tmp.getMinMiddle() * 2.7 < 0) {
+                        if (lessThan(tmp.getMiddle(), 0.06) && tmp.getMiddle() + tmp.getMinMiddle() * 1.7 < 0) {
                             return false;
                         }
                         break;
