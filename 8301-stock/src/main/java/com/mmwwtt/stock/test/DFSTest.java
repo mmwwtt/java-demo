3a6ee795eed9ba85ec9d612e1a853d50c02c79da
moweitao <2310607361@qq.com>
2026-04-13 02:27:30 +0800

    update

--- 8301-stock/src/main/java/com/mmwwtt/stock/test/DFSTest.java
+++ 8301-stock/src/main/java/com/mmwwtt/stock/test/DFSTest.java
@@ -63,7 +63,7 @@ public class DFSTest {
     /**
      * DFS 过滤策略
      */
-    public static FilterFieldEnum fieldEnum = FilterFieldEnum.RISE3_MIDDLE_40_DAY;
+    public static FilterFieldEnum fieldEnum = FilterFieldEnum.RISE3_MAX_MIDDLE_50_DAY;
 
     @Test
     @DisplayName("DFS深度遍历")
