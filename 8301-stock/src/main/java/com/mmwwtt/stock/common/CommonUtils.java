5cb3b3971bd3e2360f1efecdd096795eff78d740
moweitao <2310607361@qq.com>
2026-04-19 21:22:56 +0800

    Revert "update"
    This reverts commit 8e9cd3b4b2efc27d31d8d5e9e2ee0813b0e25064.

--- 8301-stock/src/main/java/com/mmwwtt/stock/common/CommonUtils.java
+++ 8301-stock/src/main/java/com/mmwwtt/stock/common/CommonUtils.java
@@ -350,7 +350,8 @@ public class CommonUtils {
      * 前提：两个list 都要升序,且无重复
      */
     public static int[] retainAll(int[] list1, int[] list2) {
-        if (ArrayUtils.isEmpty(list1) || ArrayUtils.isEmpty(list2)) {
+        if (Objects.isNull(list1) || list1.length == 0 ||
+                Objects.isNull(list2) || list2.length == 0) {
             return ArrayUtils.EMPTY_INT_ARRAY;
         }
         boolean isBig = Math.max(list1.length, list2.length) * 1.0 / Math.min(list1.length, list2.length) > 10;
@@ -367,19 +368,20 @@ public class CommonUtils {
         int[] tmpArr = new int[Math.min(list1.length, list2.length)];
         int j = 0;
         int arrIdx = 0;
-        for (int num1 : list1) {
+        for (int i = 0; i < list1.length; i++) {
+            int num1 = list1[i];
             while (num1 > list2[j]) {
                 j++;
                 if (j >= list2.length) {
-                    break;
+                    return Arrays.copyOfRange(tmpArr, 0, arrIdx);
                 }
             }
             if (num1 == list2[j]) {
                 tmpArr[arrIdx] = num1;
                 j++;
                 arrIdx++;
-                if (j >= list2.length) {
-                    break;
+                if (j >= list2.length || i == list1.length - 1) {
+                    return Arrays.copyOfRange(tmpArr, 0, arrIdx);
                 }
             }
         }
